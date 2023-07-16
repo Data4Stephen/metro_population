a6d2e6ff653c9a6987724d6ba5c785ad555e6ec5
Data4Stephen <139660501+Data4Stephen@users.noreply.github.com>
2023-07-16 12:24:50 -0400

    Add files via upload

---
+++ db_config_template.py
@@ -0,0 +1,13 @@
+## AWS POSTGRES DB DETAILS
+host = "CONNECTION STRING"
+port = "PORT"
+user = "DATABASE USER"
+password = "DATABASE PASSWORD
+database = "DATABASE NAME"
+
+# define database table names
+population_table = 'TABLE NAME'
+msa_table = 'TABLE NAME'
+
+# Census Bureau API Key
+api_key = "CENSUS API KEY"
